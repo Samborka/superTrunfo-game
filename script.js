403817a2b037e27a0b9e5a8fba1c7ba87b34db9e
Henrique Alves <henriqueadlima@hotmail.com>
2021-09-28 19:31:33 -0300

    first-commit
    Feito na imersão dev com algumas modificações pessoais

---
+++ script.js
@@ -0,0 +1,409 @@
+var carta1 = {
+  nome: "Sasuke",
+  imagem:
+    "https://criticalhits.com.br/wp-content/uploads/2020/08/Sasukes_Ninja_Way.jpg",
+  atributos: {
+    ataque: 10,
+    defesa: 4,
+    chackra: 8
+  }
+};
+
+var carta2 = {
+  nome: "Naruto",
+  imagem:
+    "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/ca2e913adf3055cbb60624e4fbd9e3d98f3fe16b45e8c17d21886c4ba4a8df59._RI_V_TTW_.png",
+  atributos: {
+    ataque: 8,
+    defesa: 5,
+    chackra: 10
+  }
+};
+var carta3 = {
+  nome: "Sakura",
+  imagem:
+    "https://criticalhits.com.br/wp-content/uploads/2020/09/sakura-boruto.jpg",
+  atributos: {
+    ataque: 5,
+    defesa: 10,
+    chackra: 8
+  }
+};
+var carta4 = {
+  nome: "Kakashi",
+  imagem:
+    "https://geeksaw.com.br/wp-content/uploads/2016/07/Kakashi_Hatake.png",
+  atributos: {
+    ataque: 9,
+    defesa: 8,
+    chackra: 7
+  }
+};
+var carta5 = {
+  nome: "Gaara",
+  imagem:
+    "https://kanto.legiaodosherois.com.br/w750-h450-k1/wp-content/uploads/2021/04/legiao_4KMf1YOWSZew.png.jpeg",
+  atributos: {
+    ataque: 7,
+    defesa: 8,
+    chackra: 8
+  }
+};
+var carta6 = {
+  nome: "Madara",
+  imagem:
+    "https://sm.ign.com/ign_br/screenshot/default/madara-plano-olho-da-lua_rtrx.jpg",
+  atributos: {
+    ataque: 9,
+    defesa: 9,
+    chackra: 9
+  }
+};
+var carta7 = {
+  nome: "Itachi",
+  imagem:
+    "https://nerdhits.com.br/wp-content/uploads/2021/07/tumulo-itachi-uchiha-konoha-naruto-memorial-1200x720.jpg",
+  atributos: {
+    ataque: 8,
+    defesa: 7,
+    chackra: 5
+  }
+};
+var carta8 = {
+  nome: "Obito",
+  imagem:
+    "https://static.quizur.com/i/b/585debbbbf0795.89186372Obito-Uchiha.png",
+  atributos: {
+    ataque: 9,
+    defesa: 8,
+    chackra: 8
+  }
+};
+var carta9 = {
+  nome: "Minato",
+  imagem:
+    "https://criticalhits.com.br/wp-content/uploads/2019/02/minato-kunai-01.jpg",
+  atributos: {
+    ataque: 9,
+    defesa: 8,
+    chackra: 8
+  }
+};
+var carta10 = {
+  nome: "Hinata",
+  imagem:
+    "https://criticalhits.com.br/wp-content/uploads/2021/09/hinata-hyuga-naruto.jpg",
+  atributos: {
+    ataque: 6,
+    defesa: 8,
+    chackra: 6
+  }
+};
+var carta11 = {
+  nome: "Tsunade",
+  imagem:
+    "https://criticalhits.com.br/wp-content/uploads/2018/12/Byakug%C5%8D_no_Jutsu.png",
+  atributos: {
+    ataque: 7,
+    defesa: 9,
+    chackra: 7
+  }
+};
+var carta12 = {
+  nome: "Jiraya",
+  imagem:
+    "https://sm.ign.com/ign_br/screenshot/default/jiraiya-naruto-1_b6rx.jpg",
+  atributos: {
+    ataque: 8,
+    defesa: 6,
+    chackra: 7
+  }
+};
+var carta13 = {
+  nome: "Orochimaru",
+  imagem:
+    "https://criticalhits.com.br/wp-content/uploads/2019/12/orochimaru.jpg",
+  atributos: {
+    ataque: 7,
+    defesa: 7,
+    chackra: 6
+  }
+};
+var carta14 = {
+  nome: "Shikamaru",
+  imagem:
+    "https://criticalhits.com.br/wp-content/uploads/2019/01/Shikamaru_Nara.png",
+  atributos: {
+    ataque: 7,
+    defesa: 6,
+    chackra: 5
+  }
+};
+var carta15 = {
+  nome: "Guy",
+  imagem:
+    "https://criticalhits.com.br/wp-content/uploads/2019/01/Might_Guy.png",
+  atributos: {
+    ataque: 8,
+    defesa: 7,
+    chackra: 2
+  }
+};
+var carta16 = {
+  nome: "Rock Lee",
+  imagem: "https://criticalhits.com.br/wp-content/uploads/2020/01/rock-lee.jpg",
+  atributos: {
+    ataque: 8,
+    defesa: 5,
+    chackra: 2
+  }
+};
+var carta17 = {
+  nome: "Temari",
+  imagem:
+    "https://nerdhits.com.br/wp-content/uploads/2021/05/temari-naruto-1-1200x675.jpg",
+  atributos: {
+    ataque: 6,
+    defesa: 6,
+    chackra: 4
+  }
+};
+var carta18 = {
+  nome: "Neji",
+  imagem:
+    "https://nerdhits.com.br/wp-content/uploads/2021/07/neji-hyuga-naruto-1200x900.jpg",
+  atributos: {
+    ataque: 7,
+    defesa: 6,
+    chackra: 5
+  }
+};
+var carta19 = {
+  nome: "Ino",
+  imagem:
+    "https://criticalhits.com.br/wp-content/uploads/2021/08/Ino-Yamanaka-cosplay.jpg",
+  atributos: {
+    ataque: 5,
+    defesa: 6,
+    chackra: 5
+  }
+};
+var carta20 = {
+  nome: "Choji",
+  imagem:
+    "https://i.pinimg.com/originals/31/46/b2/3146b258993ebd730908528a17a5c92e.png",
+  atributos: {
+    ataque: 5,
+    defesa: 6,
+    chackra: 4
+  }
+};
+var baralhoJogador = [
+  carta1,
+  carta2,
+  carta3,
+  carta4,
+  carta5,
+  carta6,
+  carta7,
+  carta8,
+  carta9,
+  carta10,
+  carta11,
+  carta12,
+  carta13,
+  carta14,
+  carta15,
+  carta16,
+  carta17,
+  carta18,
+  carta19,
+  carta20
+];
+var baralhoMaquina = [
+  carta1,
+  carta2,
+  carta3,
+  carta4,
+  carta5,
+  carta6,
+  carta7,
+  carta8,
+  carta9,
+  carta10,
+  carta11,
+  carta12,
+  carta13,
+  carta14,
+  carta15,
+  carta16,
+  carta17,
+  carta18,
+  carta19,
+  carta20
+];
+
+var cartaMaquina;
+var cartaJogador;
+
+function sortearCarta() {
+  var numeroCartaMaquina = parseInt(Math.random() * 20);
+  cartaMaquina = baralhoMaquina[numeroCartaMaquina];
+
+  var numeroCartaJogador = parseInt(Math.random() * 20);
+  while (numeroCartaMaquina == numeroCartaJogador) {
+    numeroCartaJogador = parseInt(Math.random() * 20);
+  }
+  cartaJogador = baralhoJogador[numeroCartaJogador];
+
+  document.getElementById("btnSortear").disabled = true;
+  document.getElementById("btnJogar").disabled = false;
+
+  exibirCartaJogador();
+}
+
+function obtemAtributoSelecionado() {
+  //Traz todos os radios buttons
+  var radioAtributos = document.getElementsByName("atributo");
+
+  for (var i = 0; radioAtributos.length; i++) {
+    if (radioAtributos[i].checked == true) {
+      return radioAtributos[i].value;
+    }
+  }
+}
+
+function jogar() {
+  var atributoSelecionado = obtemAtributoSelecionado();
+  var elementoDivResultado = document.getElementById("resultado");
+  var elementoNumeroCartasRecebidas = document.getElementById("numeroCartas");
+  var numeroRodadasVencidas = 0;
+  var numeroRodadasPerdidas = 0;
+  var valorAtributoSelecionadoJogador =
+    cartaJogador.atributos[atributoSelecionado];
+  var valorAtributoSelecionadoMaquina =
+    cartaMaquina.atributos[atributoSelecionado];
+
+  if (valorAtributoSelecionadoJogador > valorAtributoSelecionadoMaquina) {
+    divResultado =
+      "<p class='resultado-final'>Você venceu! O valor da carta da máquina era: " +
+      valorAtributoSelecionadoMaquina +
+      "</p>";
+
+    baralhoMaquina.pop(cartaMaquina);
+    baralhoJogador.push(cartaMaquina);
+    elementoNumeroCartasRecebidas.innerHTML =
+      "Número de cartas do seu baralho: " + baralhoJogador.length;
+  } else if (
+    valorAtributoSelecionadoMaquina > valorAtributoSelecionadoJogador
+  ) {
+    divResultado =
+      "<p class='resultado-final'>Você perdeu! O valor da carta da máquina era: " +
+      valorAtributoSelecionadoMaquina +
+      "</p>";
+    baralhoJogador.pop(cartaJogador);
+    baralhoMaquina.push(cartaJogador);
+    elementoNumeroCartasRecebidas.innerHTML =
+      "Número de cartas do seu baralho: " + baralhoJogador.length;
+  } else {
+    divResultado =
+      "<p class='resultado-final'>Você empatou! O valor da carta da máquina era: " +
+      valorAtributoSelecionadoMaquina +
+      "</p>";
+  }
+  elementoDivResultado.innerHTML = divResultado;
+
+  document.getElementById("btnJogar").disabled = true;
+  document.getElementById("btnJogarNovamente").disabled = false;
+  exibirCartaMaquina();
+  definirVencedor();
+}
+
+function jogarNovamente() {
+  var divCartaJogador = document.getElementById("carta-jogador");
+  var divMaquina = document.getElementById("carta-maquina");
+  var moldura =
+    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  divCartaJogador.style.backgroundImage = null;
+  divCartaJogador.innerHTML = moldura;
+  divMaquina.style.backgroundImage = null;
+  divMaquina.innerHTML = moldura;
+
+  document.getElementById("btnSortear").disabled = false;
+  document.getElementById("btnJogar").disabled = true;
+  document.getElementById("btnJogarNovamente").disabled = true;
+}
+
+function exibirCartaJogador() {
+  var divCartaJogador = document.getElementById("carta-jogador");
+  divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
+  //Faz o mesmo que a linha de cima, mas a notação de cima é mais usada, pois mostra o que está sendo feito(mistura de css com JS)
+  //divCartaJogador.style.backgroundImage = "url(" + cartaJogador.imagem + ")"
+
+  var moldura =
+    "<img src= 'https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png' style=' width: inherit; height: inherit; position: absolute'>";
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+  var colocarOpcoesNaTela = "";
+
+  for (var atributo in cartaJogador.atributos) {
+    colocarOpcoesNaTela +=
+      "<input type='radio' name='atributo' id= 'atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaJogador.atributos[atributo] +
+      "<br>" +
+      "</input>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`;
+
+  divCartaJogador.innerHTML =
+    moldura + nome + tagHTML + colocarOpcoesNaTela + "</div>";
+}
+
+function exibirCartaMaquina() {
+  var divCartaMaquina = document.getElementById("carta-maquina");
+  divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
+  //Faz o mesmo que a linha de cima, mas a notação de cima é mais usada, pois mostra o que está sendo feito(mistura de css com JS)
+  //divCartaMaquina.style.backgroundImage = "url(" + cartaMaquina.imagem + ")"
+
+  var moldura =
+    "<img src= 'https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png' style=' width: inherit; height: inherit; position: absolute'>";
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+  var colocarOpcoesNaTela = "";
+
+  for (var atributo in cartaMaquina.atributos) {
+    colocarOpcoesNaTela +=
+      "<p type='text' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaMaquina.atributos[atributo] +
+      "</p>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`;
+
+  divCartaMaquina.innerHTML =
+    moldura + nome + tagHTML + colocarOpcoesNaTela + "</div>";
+}
+
+function definirVencedor() {
+  var elementoDivResultadoFinal = document.getElementById("resultado");
+  if (baralhoMaquina.length == 17) {
+    divResultadoFinal = "<p class='resultado-final'>Você venceu a partida.</p>";
+    document.getElementById("btnSortear").disabled = true;
+    document.getElementById("btnJogar").disabled = true;
+    document.getElementById("btnJogarNovamente").disabled = true;
+  } else if (baralhoJogador.length == 17) {
+    divResultadoFinal =
+      "<p class='resultado-final'>A máquina venceu a partida.</p>";
+    document.getElementById("btnSortear").disabled = true;
+    document.getElementById("btnJogar").disabled = true;
+    document.getElementById("btnJogarNovamente").disabled = true;
+  }
+  elementoDivResultadoFinal.innerHTML = divResultadoFinal;
+}
